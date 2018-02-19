8efcf69beb8e59b287b86ffd883c002bc0252755
Won Ryul Lee <wlee367@uwo.ca>
2018-02-19 01:42:06 -0500

    currently the coursecode does not get input correctly into the data, but i think i can fix it easily tomorrow

--- scraper/scrape.py
+++ scraper/scrape.py
@@ -13,13 +13,12 @@ def to_military(m):
 
 url = "http://studentservices.uwo.ca/secure/timetables/mastertt/ttindex.cfm"
 
-chromedriver = "/Users/Josh/Desktop/chromedriver"
+chromedriver = "/Users/jasonlee/CS4471/scraper/chromedriver"
 
 driver = webdriver.Chrome(chromedriver)
 driver.get(url)
 
-courseCodes = ["ACTURSCI","AMERICAN","ANATCELL","ANTHRO","APPLMATH","ARABIC","ARTHUM","ASTRONOM","BIBLSTUD","BIOCHEM","BIOLOGY","BIOPHYS","BIOSTATS","MTP-TVSN","BUSINESS","CALCULUS","CGS","CBE","CHEMBIO","CHEM","CSI","CHINESE","CHURCH","CHURLAW","CHURMUSI","CEE","CLASSICS","CMBPROG","COMMSCI","COMPLIT","COMPSCI","DANCE","DIGICOMM","DOL","DISABST","EARTHSCI","ECONOMIC","EELC","ECE","ENGSCI","ENGLISH","ENVIRSCI","EPID","EPIDEMIO","FAMLYSTU","FLDEDUC","FILM","FINMOD","FRSTNATN","FOODNUTR","FRENCH","GEOGRAPH","GEOLOGY","GERMAN","GREEK","GPE","HEALTSCI","HEBREW","HINDI","HISTTHEO","HISTORY","HISTSCI","HOMILET","HUMANECO","INTEGSCI","INTERDIS","INTREL","ITALIAN","JAPANESE","JEWISH","MTP-BRJR","KINESIOL","LATIN","LAW","LINGUIST","LITURST","LITURGIC","MOS","MTP-MKTG","MATH","MME","MIT","MEDBIO","MEDHINFO","MEDSCIEN","MEDIEVAL","MICROIMM","MORALTHE","MTP-MMED","MUSIC","NEURO","NURSING","PASTTHEO","PATHOL","PERSIAN","PHARM","PHILST","PHILOSOP","PHYSICS","PHYSIOL","POLISCI","PORTUGSE","PSYCHOL","REHABSCI","RELEDUC","RELSTUD","RUSSIAN","SACRTHEO","SCHOLARS","SCIENCE","SOCLJUST","SOCWORK","SOCIOLOG","SE","SPANISH","SPEECH","SPIRTHEO","STATS","SYSTHEO","THANAT","THEATRE","THEOETH","THEOLST","THESIS","TJ","VAHISTRY","VASTUDIO","WTC","WOMENST","WRITING"]
-
+courseCodes = ["ACTURSCI", "AMERICAN", "ANATCELL", "ANTHRO", "APPLMATH", "ARABIC", "ARTHUM", "ASTRONOM", "BIBLSTUD", "BIOCHEM", "BIOLOGY", "BIOSTATS", "MTP-RADO", "MTP-TVSN", "MTP-FLDP", "BUSINESS", "CALCULUS", "CGS", "CBE", "CHEMBIO", "CHEM", "CSI", "CHINESE", "CHURCH", "CHURLAW", "CHURMUSI", "CEE", "CLASSICS", "CMBPROG", "COMMSCI", "COMPLIT", "COMPSCI", "DANCE", "DIGICOMM", "MTP-DIGL", "DIGIHUM", "DOL", "DISABST", "EARTHSCI", "ECONOMIC", "EELC", "ECE", "ENGSCI", "ENGLISH", "ENVIRSCI", "EPID", "EPIDEMIO", "FAMLYSTU", "FLDEDUC", "MTP-FILM", "FILM", "FINMOD", "FRSTNATN", "FOODNUTR", "FRENCH", "GEOGRAPH", "GEOLOGY", "GEOPHYS", "GERMAN", "GREEK", "GPE", "HEALTSCI", "HEBREW", "HINDI", "HISTTHEO", "HISTORY", "HISTSCI", "HOMILET", "HUMANECO", "INTEGSCI", "ICC", "INTERDIS", "INTREL", "ITALIAN", "JAPANESE", "JEWISH", "MTP-BRJR", "KINESIOL", "LATIN", "LAW", "LINGUIST", "LITURST", "LITURGIC", "MOS", "MTP-MKTG", "MATH", "MME", "MSE", "MIT", "MEDBIO", "MEDHINFO", "MEDSCIEN", "MEDIEVAL", "MICROIMM", "MORALTHE", "MTP-MMED", "MUSIC", "NEURO", "NURSING", "PASTTHEO", "PATHOL", "PERSIAN", "PHARM", "PHILST", "PHILOSOP", "PHYSICS", "PHYSIOL", "PHYSPHRM", "POLISCI", "PORTUGSE", "PSYCHOL", "REHABSCI", "RELEDUC", "RELSTUD", "SACRTHEO", "SCHOLARS", "SCIENCE", "SOCLJUST", "SOCWORK", "SOCIOLOG", "SE", "SPANISH", "SPEECH", "SPIRTHEO", "STATS", "SUPPAST", "SYSTHEO", "THANAT", "THEATRE", "THEOETH", "THEOLST", "THESIS", "TJ", "VAHISTRY", "VASTUDIO", "WTC", "WOMENST", "WORLDLIT", "WRITING"] 
 data = []
 
 # Where H is Thursday.
@@ -29,6 +28,8 @@ f = csv.writer(open("Data.csv", "w"))
 data.append(["Course Code Start End Location Days"])
 
 for j, code in enumerate(courseCodes):
+
+    # data.append(code)
     # Take a break after every scrape otherwise we run into a captcha.
     time.sleep(10)
     print(code)
@@ -41,23 +42,25 @@ for j, code in enumerate(courseCodes):
     soup = BeautifulSoup(html, "html.parser")
 
     table = soup.find(class_='table table-striped')
-
+    captions = soup.find_all('h4')
+    test = []
+    for caption in captions:
+        strings = caption.text.split()
+        course = strings[0]
+        code = strings[1]
+        test.append(course + " " + code )
+    # print(test)
+         
     for t in soup.find_all('table'):
+        body = t.find_all('tr')
+        for row in body:
 
-        try:
-            caption = t.find_all('caption')
-            split = caption[0].text.split()
-            course = split[0]
-            code = split[1]
-        except:
-            pass
-
-        for row in t.find_all('tr'):
             a = []
             l = 0
             daySequence = ""
             for i, col in enumerate(row.find_all('td')):
                 if i > 3 and i < 9:
+
                     if col.contents[0] and col.contents[0].strip():
                         daySequence = daySequence + days[i - 4]
                 elif i > 8 and i < 12:
@@ -68,6 +71,20 @@ for j, code in enumerate(courseCodes):
                         pass
 
             if l == 3:
-                data.append([split[0] + " " + split[1] + " " + to_military(a[0]) + " " + to_military(a[1]) + " " + a[2] + " " + daySequence])
 
-f.writerows(data)
+                # (course+ " "+ code+ " " + a[0]+" "+a[1]+ " " + a[2] + " " + daySequence)
+                # data.append([split[0] + " " + split[1] + " " + to_military(a[0]) + " " + to_military(a[1]) + " " + a[2] + " " + daySequence]
+                data.append("course"+ " " + a[0]+" "+a[1]+ " " + a[2] + " " + daySequence)
+    # print(data)
+
+## in the test array lies all the course and code entries
+    for i in data:
+        for j in test:
+            if 'course' in data:
+                data[i] == test[j]
+    print(data)
+
+    f.writerows(data)
+
+
+
