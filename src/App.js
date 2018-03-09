b5acb2a46a69349cbbbc3c994757a0d83a1385fa
Jose Rivera <jrivera4@uwo.ca>
2018-03-09 02:17:31 -0500

    SPA

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+    Route,
+    NavLink,
+    HashRouter
+} from "react-router-dom"
+import Home from "./Home"
+import Profile from "./Profile"
+import Settings from "./Settings"
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Western's Room Booker</h1>
-        </header>
-        <p className="App-intro">
-          Please choose a building or room or something.....
-        </p>
-      </div>
+        <HashRouter>
+            <div>
+                <h1 className="title">Western Room Booker</h1>
+                <ul className="header">
+                    <li><NavLink exact to="/">Home</NavLink></li>
+                    <li><NavLink to="/profile">Profile</NavLink></li>
+                    <li><NavLink to="/settings">Settings</NavLink></li>
+                </ul>
+                <div className="content">
+                    <Route exact path="/" component={Home}/>
+                    <Route path="/profile" component={Profile}/>
+                    <Route path="/settings" component={Settings}/>
+                </div>
+            </div>
+        </HashRouter>
     );
   }
 }
