fb2ff04babc53a99b449ddbbc29663ce846c7f07
Jose Rivera <jrivera4@uwo.ca>
2018-03-15 09:49:42 -0400

    Search Page Added

--- src/Profile.js
+++ src/Profile.js
@@ -7,8 +7,11 @@ class Profile extends Component {
                 <h2>PROFILE</h2>
                 <hr/>
                 <h3>First Name:</h3><br/>
+                <h2>Jose</h2>
                 <h3>Last Name:</h3><br/>
-                <h3>User Email:</h3>
+                <h2>Rivera</h2><br/>
+                <h3>User Email:</h3><br/>
+                <h2>jrivera4@uwo.ca</h2>
             </div>
         );
     }
