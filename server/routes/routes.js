a9681dface75bc96a51fcbb390d4a3240eba524e
Sangeet Parashar <sparasha@uwo.ca>
2018-04-03 19:54:18 -0400

    sending an option of buildings to client side

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,42 +1,53 @@
 var express = require('express');
 var router = express.Router();
-const { mongoose } = require('../db/db');
-const { Room } = require('../models/Room');
-const { User } = require('../models/User');
+const {
+  mongoose
+} = require('../db/db');
+const {
+  Room
+} = require('../models/Room');
+const {
+  User
+} = require('../models/User');
 const assert = require('assert');
 const MongoClient = require('mongodb').MongoClient;
 
-MongoClient.connect('mongodb://admin:admin@ds155278.mlab.com:55278/4471', function(err, database) {
+MongoClient.connect('mongodb://admin:admin@ds155278.mlab.com:55278/4471', function (err, database) {
 
-   assert.equal(null, err);
-    console.log("Successfully connected to MongoDB.");
-    const db = database.db('4471');
+  assert.equal(null, err);
+  console.log("Successfully connected to MongoDB.");
+  const db = database.db('4471');
+  let location = [];
+  router.get('/', function (req, res) {
+    db.collection('Courses').find().toArray(
+      function (err, result){
+        if(err)
+          console.log(err);
+        result.forEach((record) => {
+          location.push(record.building);
+        });
+      uniqueArray = location.filter(function(elem, pos) {
+        return location.indexOf(elem) == pos;
+      });
+      // console.log(uniqueArray);
+      res.render({
+        "buildings": uniqueArray
+        });
+      });
 
-   router.get('/', function(req, res){
-    db.collection('Courses').aggregate([{$group: {"_id":"$building"}}], function(err, result){
-      if(err){
-        console.log(err);
-      }else{
-       console.log(result);
+      
+    
+    db.collection('Courses').find({
+      start: '9:30 AM'
+    }).toArray(
+      function (err, result) {
+        res.render('index.ejs', {
+          'Courses': result
+        });
+        // db.Courses.aggregate([{$group: {"_id":"$building"}}])
       }
-    });
-    // console.log(query);
-      db.collection('Courses').find({ start: '9:30 AM'}).toArray(
-        function(err, result){
-          res.render('index.ejs', {'Courses': result});
-          // db.Courses.aggregate([{$group: {"_id":"$building"}}])
-         }
-      );
-   });
-   router.get('/#/main/search', function(req, res){
-     var query = db.collection('Courses').aggregate([{$group: {"_id":"$building"}}])
-     console.log(query)
-    //  .toArray(
-    //    function(err, result){
-    //      res.send({'Buildings': result});
-    //      console.log(result);
-    //    });
-   });
+    );
+  });
 });
 
 module.exports = router;
