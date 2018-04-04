d46ff9c4fc8c9f0dde25abcec04518f730bdc53f
Won Ryul Lee <wlee367@uwo.ca>
2018-04-04 12:56:26 -0400

    uploading search.js for sangeet to look at, added axios to package.json, please npm install, and added bundle.js to .gitingore

--- client/Search.js
+++ client/Search.js
@@ -5,16 +5,41 @@ import {
     HashRouter
 } from "react-router-dom";
 import Room from "./Room";
+import axios from 'axios';
 
-class Search extends Component {
+
+class Search extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            buildings:[]
+        }
+        console.log(this.state);
+        this.render = this.render.bind(this.state.buildings);
+    }
+    componentDidMount(){
+        axios.get('/API/search')
+        .then(resp=> {
+            // console.log(resp.data);
+            this.setState({
+                buildings: resp.data,
+            })
+        }).catch(console.error);
+    }
     render() {
+        // console.log(typeof(this.state.buildings));
         return (
             <div>
                 <h2>SEARCH</h2>
                 <hr/>
                 <h3>Please choose a building:</h3>
                 <select>
-                    
+                    <option>
+
+                        {this.state.buildings.map((build)=>{
+                            <option>build</option>
+                        })};
+                    </option>
                     <option selected>Building...</option>
                 </select>
 
