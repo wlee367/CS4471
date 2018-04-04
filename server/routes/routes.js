b3ee49420711b990578a041e0d43a29b3d6a1145
Won Ryul Lee <wlee367@uwo.ca>
2018-04-04 17:53:01 -0400

    adding a post request and a get mehtod at /api/building to put all the stuff there

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -13,16 +13,43 @@ MongoClient.connect('mongodb://admin:admin@ds155278.mlab.com:55278/4471', functi
   const db = database.db('4471');
   let location = [];
   let uniqueArray = [];
+  let uniqueRoomArray =[];
+  let building_to_query;
+  let room = [];
+  let query_for_rooms;
 
   router.get('/', function (req, res) {
         res.render('index.ejs');
-      
   });
   router.post('/', function (req, res) {
-    res.send('got the data we needed');
-    console.log(req.body);
-  
+    // res.send('got the data we needed');
+    console.log(req.body.value);
+    building_to_query = req.body.value;
+    console.log('wait')
+    // building_to_query.push(req.body);
+    console.log(building_to_query);
+    query_for_rooms = {"building": building_to_query};
 });
+
+// console.log(building_to_query);
+  router.get('/API/building', function(req, res){
+    db.collection('Courses').find(query_for_rooms).toArray(
+      function(err, result){
+        console.log(query_for_rooms);
+        if(err)
+          console.log(err);
+        result.forEach((record)=>{
+          room.push(record.room);
+          // console.log(record.room);
+          // console.log(room);
+        });
+      uniqueRoomArray = room.filter(function(elem,pos){
+        return location.indexOf(elem) == pos;
+      });
+      res.send({rooms:uniqueRoomArray});
+      //res.send(result);
+    });
+  })
 
   router.get('/API/search', function(req,res) {
     db.collection('Courses').find().toArray(
